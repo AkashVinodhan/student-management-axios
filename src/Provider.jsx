11ab53ceaed5f2ca4565fc26e12994bcd3058137
AkashVinodhan <akashvinodhan@gmail.com>
2023-04-13 08:25:20 +0530

    initial commit

---
+++ src/Provider.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Context from "./Context";
+
+export default function Provider(props) {
+  const [students, setStudents] = useState([]);
+
+  const fetchStudents = () => {
+    axios
+      .get("https://64340404582420e231718e94.mockapi.io/students")
+      .then((res) => setStudents(res.data));
+  };
+
+  const createStudent = (data) => {
+    axios
+      .post("https://64340404582420e231718e94.mockapi.io/students/", {
+        ...data,
+      })
+      .then((res) => console.log(res.data));
+  };
+
+  const updateStudent = (id, data) => {
+    axios
+      .put("https://64340404582420e231718e94.mockapi.io/students/" + id, {
+        ...data,
+      })
+      .then((res) => console.log(res.data));
+  };
+
+  const deleteStudent = (id) => {
+    axios
+      .delete("https://64340404582420e231718e94.mockapi.io/students/" + id)
+      .then((res) => fetchStudents());
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        students,
+        setStudents,
+        fetchStudents,
+        createStudent,
+        updateStudent,
+        deleteStudent,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+}
